4f1933863c84776b5f0dc6b34f46dfad7a8d4e1c
Laura Djerf <djerf.laura@gmail.com>
2023-09-19 00:40:42 +0300

    Implement guessing
    Manages app state and combines guess results and form.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+
+import GuessInput from "../GuessInput/GuessInput";
+import GuessResults from "../GuessResults/GuessResults";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
+
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guesses, setGuesses] = React.useState([]);
+
+  function handleSubmitGuesses(tentativeGuess) {
+    setGuesses([...guesses, tentativeGuess]);
+  }
+
+  return (
+    <>
+      <GuessResults guesses={guesses} />
+      <GuessInput handleSubmitGuesses={handleSubmitGuesses} />
+    </>
+  );
 }
 
 export default Game;
