63f3245f2ef3a63f4ec467a7fdd841a7519007a5
Laura Djerf <laura.djerf@lianatech.com>
2023-09-19 21:26:14 +0300

    Add Banner component
    Returns a banner that dynamically changes styles with the status prop

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Banner({ status, children }) {
+  return (
+    <div className={`${status} banner`}>
+      <p>{children}</p>
+    </div>
+  );
+}
+
+export default Banner;
